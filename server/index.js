4ddb72e3b19f693fa507efb4e71ad45b54bf8437
Taylor <taylor.sheets3@gmail.com>
2022-11-14 15:56:45 -0800

    Dependencies finished, verified server works, webpack/babel work.  Ready to begin app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, /*TODO define path*/)));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 
 /*TODO define routes*/
